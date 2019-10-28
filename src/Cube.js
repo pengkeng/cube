bac49886c6670486ddf5f6cca3d9790b69ff8a2c
彭前程 <936506220@qq.com>
2019-10-28 14:54:55 +0800

    设置角度

--- src/Cube.js
+++ src/Cube.js
@@ -102,6 +102,7 @@ function Cube(id, opts) {
 
     //给容器加旋转事件
     this.containerMouseMove();
+
     this.state = "UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB";
 
 }
@@ -172,6 +173,7 @@ Cube.prototype.initStyle = function () {
         }
 
     }
+    this.initL();
 };
 //干掉魔方上所有颜色的方法，，只剩下材料颜色
 Cube.prototype.delColor = function () {
@@ -380,7 +382,9 @@ Cube.prototype.containerMouseMove = function () {
 };
 //初始化魔方位子
 Cube.prototype.initL = function () {
-    this.rotateX = this.rotateY = this.rotateZ = 0;
+    this.rotateX = -30;
+    this.rotateY = 30;
+    this.rotateZ = 0;
     this.container.style.WebkitTransform = 'perspective(800px) rotateY(' + this.rotateY + 'deg) rotateX(' + this.rotateX + 'deg) rotateZ(' + this.rotateZ + 'deg)';
 };
 //设置容器拖拽时候的鼠标灵敏度
